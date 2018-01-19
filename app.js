5db830d0e12f33f769b6ec8f86ef2fbddf44c2cd
David Stoll <dstoll243@gmail.com>
2018-01-18 18:19:19 -0800

    fixed linter errors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var userPoints = 0;
 
 alert('Hello! My name is David and I have crafted a guessing game for us to play!');
 
-var user = prompt('First off, what is your name?')
+var user = prompt('First off, what is your name?');
 
 alert('Well hello ' + user + '! Let\'s find out how well you know me.');
 
@@ -32,13 +32,13 @@ if (answer2 === 'yes' || answer2 === 'y'){
   userPoints++;
   alert('Unfortunately, yes, I am a banker');
 } else {
-  alert('Sorry to say, I\'ve been in banking for 10.5 years')
+  alert('Sorry to say, I\'ve been in banking for 10.5 years');
 }
 
 alert('You have ' + userPoints + ' points!');
 console.log(user + ' has a score of ' + userPoints);
 
-alert('On to the next one!')
+alert('On to the next one!');
 
 var answer3 = prompt('Is David\'s favorite baseball team the Seattle Mariners?' , 'yes or no').toLowerCase();
 console.log('Is David\'s favorite baseball team the Seattle Mariners?');
@@ -47,7 +47,7 @@ console.log(answer3);
 if (answer3 === 'yes' || answer3 === 'y'){
   userPoints++;
   alert('You\'d better believe it.');
-} else {alert('I\'m disappointed that you would think that of me.')
+} else {alert('I\'m disappointed that you would think that of me.');
 }
 
 alert('You have ' + userPoints + ' points!');
@@ -56,16 +56,16 @@ console.log(user + ' has a score of ' + userPoints);
 alert('We\'re almost there!');
 
 var answer4 = prompt('Is there any food that David does not like?' , 'yes or no').toLowerCase();
-console.log('Is there any food that David does not like?')
+console.log('Is there any food that David does not like?');
 console.log(answer4);
 
 if (answer4 === 'yes' || answer4 === 'y'){
   userPoints++;
   alert('Correct. While I\'ll eat most anything, I really don\'t like beets.');
-} else {alert('It may surprise you, but I REALLY don\'t like beets')
+} else {alert('It may surprise you, but I REALLY don\'t like beets');
 }
 
-alert('Dwight Schrute and I would not get along')
+alert('Dwight Schrute and I would not get along');
 
 alert('You have ' + userPoints + ' points!');
 console.log(user + ' has a score of ' + userPoints);
@@ -79,7 +79,7 @@ console.log(answer5);
 if (answer5 === 'yes' || answer5 === 'y'){
   userPoints++;
   alert('Definitely. I\'d love to watch the Utah Jazz play up here a few times a year.');
-} else {alert('While I was never a Sonics fan, I definitely wish they were still here')
+} else {alert('While I was never a Sonics fan, I definitely wish they were still here');
 }
 
 alert('Another question for you');
